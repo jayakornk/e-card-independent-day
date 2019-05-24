149b5168d612477a985a6ac6ae9fd181f7af6959
Bacon <bacon@prontomarketing.com>
2019-05-24 23:43:21 +0700

    🐛 transition and drag movement 🥓

--- script.js
+++ script.js
@@ -20,37 +20,28 @@ interact('.drop-here')
     ondragleave: function (event) {
       event.target.classList.remove('activate');
     },
-    // ondrop: function (event) {
-      //   event.target.classList.add('activated');
-      // },
+    ondrop: function (event) {
+      const related = event.relatedTarget;
+
+      related.classList.add('animate-drop');
+
+      setTimeout(function() {
+        related.classList.remove('animate-drop');
+      }, 1050);
+    },
     ondropdeactivate: function (event) {
       event.target.classList.remove('activate');
-      event.target.classList.remove('activated')
     },
-    // checker: function (dragEvent,         // related dragmove or dragend
-    //   event,             // Touch, Pointer or Mouse Event
-    //   dropped,           // bool default checker result
-    //   dropzone,          // dropzone Interactable
-    //   dropElement,       // dropzone elemnt
-    //   draggable,         // draggable Interactable
-    //   draggableElement) {// draggable element
-    //   // only allow drops into empty dropzone elements
-    //   console.log(dropped && !dropElement.hasChildNodes());
-    //   console.log(draggableElement);
-    //   if (draggableElement.getAttribute('data-true') !== 'true') {
-    //     return false;
-    //   }
-    //   return true;
-    //   // return dropped && !dropElement.hasChildNodes();
-    // },
   })
 
 Object.keys(scenes.scene0).forEach((key, index) => {
+  const container = document.querySelector('.container');
+  const dropzone = document.querySelector('.drop-here');
+
   let el = scenes.scene0[key];
   el = parser.parseFromString(el, "image/svg+xml").documentElement;
-  console.dir(el);
   el.classList.add('dragNdrop');
-  console.log('translate(calc(-50%' + arr[index].x + 'px), calc(-50%' + arr[index].y + 'px))');
+
   el.style.webkitTransform = 'translate(calc(-50% + ' + arr[index].x + 'px), calc(-50% + ' + arr[index].y + 'px))';
   el.style.transform = 'translate(calc(-50% + ' + arr[index].x + 'px), calc(-50% + ' + arr[index].y + 'px))';
   el.setAttribute('data-x', arr[index].x);
@@ -60,9 +51,22 @@ Object.keys(scenes.scene0).forEach((key, index) => {
   if (key === 't') {
     el.setAttribute('data-true', 'true');
   }
-  document.querySelector('.container').appendChild(el);
 
-  const here = document.querySelector('.drop-here');
+  el.addEventListener('mouseover', function(event) {
+    const re = /(scale)\((.*)\)/g;
+    let scale = 1.2;
+    if (!el.style.webkitTransform.match(re)) {
+      el.style.webkitTransform += 'scale(1.2)';
+      el.style.transform += 'scale(1.2)';
+    }
+  });
+  el.addEventListener('mouseleave', function(event) {
+    const re = /(scale)(\(.*\))/g;
+    el.style.webkitTransform = el.style.transform.replace(re, '');
+    el.style.transform = el.style.transform.replace(re, '');
+  });
+  
+  container.appendChild(el);
 
   interact(el)
     .draggable({
@@ -70,9 +74,9 @@ Object.keys(scenes.scene0).forEach((key, index) => {
       snap: {
         targets: [
           {
-            x: here.offsetLeft,
-            y: here.offsetTop,
-            range: 100,
+            x: container.offsetLeft + dropzone.offsetLeft,
+            y: container.offsetTop + dropzone.offsetTop,
+            range: 200,
           },
           {
             x: arr[index].x,
@@ -81,12 +85,22 @@ Object.keys(scenes.scene0).forEach((key, index) => {
           },
         ],
         relativePoints: [
-          { x: 0.5, y: 0.5 },   // to the center
+          { x: 0.5, y: 0.5 },
         ],
         endOnly: true,
       },
       onmove: dragMoveListener,
       onend: function(event) {
+        event.target.style.transition = event.target.style.transition.replace(/(all 0s ease 0s)/, '');
+        if (!event.relatedTarget || (event.relatedTarget && !event.relatedTarget.classList.contains('drop-here')) && event.target.getAttribute('data-true') === 'true') {
+          const oriX = event.target.getAttribute('data-ori-x');
+          const oriY = event.target.getAttribute('data-ori-y');
+          event.target.style.webkitTransform = 'translate(calc(-50% + ' + oriX + 'px), calc(-50% + ' + oriY + 'px))';
+          event.target.style.transform = 'translate(calc(-50% + ' + oriX + 'px), calc(-50% + ' + oriY + 'px))';
+          event.target.setAttribute('data-x', oriX);
+          event.target.setAttribute('data-y', oriY);
+        }
+
         if (event.target.getAttribute('data-true') !== 'true') {
           const oriX = event.target.getAttribute('data-ori-x');
           const oriY = event.target.getAttribute('data-ori-y');
@@ -94,11 +108,6 @@ Object.keys(scenes.scene0).forEach((key, index) => {
           event.target.style.transform = 'translate(calc(-50% + ' + oriX + 'px), calc(-50% + ' + oriY + 'px))';
           event.target.setAttribute('data-x', oriX);
           event.target.setAttribute('data-y', oriY);
-          event.target.classList.add('wrong');
-
-          setTimeout(function() {
-            event.target.classList.remove('wrong');
-          }, 510);
         }
       }
     });
@@ -106,16 +115,13 @@ Object.keys(scenes.scene0).forEach((key, index) => {
 
 function dragMoveListener (event) {
   var target = event.target,
-      // keep the dragged position in the data-x/data-y attributes
       x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
       y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+    target.style.transition = '0s';
 
-  // translate the element
   target.style.webkitTransform =
   target.style.transform =
     'translate(calc(-50% + ' + x + 'px), calc(-50% + ' + y + 'px)';
-  // 'translate(calc(-50% + ' + arr[index].x + 'px), calc(-50% + ' + arr[index].y + 'px))';
-  // update the posiion attributes
   target.setAttribute('data-x', x);
   target.setAttribute('data-y', y);
 }
